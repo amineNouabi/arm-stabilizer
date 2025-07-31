171c1a709dc364573dd93a40c80a57703094ba33
amineNouabi <aminenouabi9@gmail.com>
2025-07-31 20:11:22 +0100

    Fixed script

--- report/Scripts/clean.sh
+++ report/Scripts/clean.sh
@@ -5,6 +5,4 @@ find . -maxdepth 1 -type f ! \( -name "*.tex" -o -name "*.cls" \) -delete
 find ./Chapters -type f ! \( -name "*.tex" \) -delete
 find ./Matter -type f ! \( -name "*.tex" \) -delete
 
-if [ -d "./_minted-IPleiriaMain" ]; then
-	rm -rf _minted-IPleiriaMain
-fi
+rm -rf _minted/
